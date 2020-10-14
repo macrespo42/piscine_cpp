77902fb7fe1463de3345087989836e6c58306762
macrespo42 <maxime.crespopro@gmail.com>
2020-10-14 15:56:37 +0200

    write main function basics

--- module_00/ex01/main.cpp
+++ module_00/ex01/main.cpp
@@ -3,5 +3,26 @@
 
 int     main(void)
 {
+    bool        exit(false);
+    std::string  command;
+
+    std::cout << "Welcome to My awesome phonebook !" << std::endl;
+    while (!exit)
+    {
+        std::cout << "Enter your command : " << std::endl;
+        getline(std::cin, command);
+        if (command == "ADD")
+        {
+            // add new contact
+        }
+        else if (command == "SEARCH")
+        {
+            // search and print a contact if it exist
+        }
+        else if (command == "EXIT")
+            exit = true;
+        else
+            std::cout << "command not found, available commands are : ADD SEARCH and EXIT" << std::endl;
+    }
     return 0;
 }
