4f545c7dfeed162a4767e5b21cce7093ffc9eaa2
macrespo42 <maxime.crespopro@gmail.com>
2020-10-22 18:02:20 +0200

    replace word (ignore space / new line atm)

---
+++ module_01/ex07/replace.hpp
@@ -0,0 +1,12 @@
+#ifndef REPLACE
+#define REPLACE
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+int error_return(std::string error_msg);
+int replace(char **av);
+
+#endif
