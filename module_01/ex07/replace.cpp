d08450e937023e353c8cc31a2a1a01dacb496ae7
macrespo42 <maxime.crespopro@gmail.com>
2020-12-16 13:51:08 +0100

    mod 1 done

--- module_01/ex07/replace.cpp
+++ module_01/ex07/replace.cpp
@@ -1,5 +1,29 @@
 #include "replace.hpp"
 
+std::string replaceLine(char *s1, char *s2, std::string& line) {
+    std::size_t s1_len(0);
+    std::size_t occ_pos = line.find(s1, 0);
+
+    for (int i(0); s1[i]; i++)
+        s1_len++;
+    while (occ_pos!=std::string::npos) {
+        line.replace(occ_pos, s1_len, s2);
+        int tmp = occ_pos;
+        occ_pos = line.find(s1, tmp);
+    }
+    return line;
+}
+
+int     count_line(char *fileName) {
+    std::ifstream file(fileName);
+    int numbersOfLines(0);
+    std::string line;
+    while (getline(file, line)) {
+        numbersOfLines++;
+    }
+    return (numbersOfLines);
+}
+
 int     main(int ac, char **av) {
     (void)av;
     if (ac == 4) {
@@ -10,7 +34,16 @@ int     main(int ac, char **av) {
         if (replacedFile) {
             std::ifstream originalFile(av[1]);
             if (originalFile) {
-                std::cout << "OK." << std::endl;
+                int numberOfLines = count_line(av[1]);
+                int i(0);
+                std::string line;
+                while (getline(originalFile, line)) {
+                    line = replaceLine(av[2], av[3], line);
+                    replacedFile << line;
+                    if (i < numberOfLines - 1)
+                        replacedFile << std::endl;
+                    i++;
+                }
             }
             else {
                 std::cout << "Error when trying to open" << av[1] << std::endl;
@@ -24,8 +57,5 @@ int     main(int ac, char **av) {
     } else {
         std::cout << "Bad arguments" << std::endl;
     }
-    // std::string base = "Hello world!";
-    // base.replace(0, 5, "Bonjour");
-    // std::cout << base << std::endl;
     return 0;
 }
