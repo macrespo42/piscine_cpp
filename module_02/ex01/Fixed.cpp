3831a20d828d4bce5701b5359ee5d3a27967ad70
macrespo42 <maxime.crespopro@gmail.com>
2020-12-17 14:18:32 +0100

    mod 02 ex01 new constructor done

---
+++ module_02/ex01/Fixed.cpp
@@ -0,0 +1,43 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void) : _fixValue(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const & src)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = src;
+}
+
+Fixed::Fixed(int const value) : _fixValue(value * 256)
+{
+    std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(float const value) : _fixValue(roundf(value * 256))
+{
+    std::cout << "Float constructor called" << std::endl;
+}
+
+Fixed::~Fixed(void) {
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_fixValue;
+}
+
+void Fixed::setRawBits(int const raw) {
+    std::cout << "setRawBits member function called" << std::endl;
+    this->_fixValue = raw;
+}
+
+Fixed & Fixed::operator=(Fixed const & src)
+{
+    std::cout << "Assignation operator called" << std::endl;
+    this->_fixValue = src._fixValue;
+    return *this;
+}
---
+++ module_02/ex01/Fixed.hpp
@@ -0,0 +1,27 @@
+#ifndef FIXED
+#define FIXED
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+
+    public:
+
+    Fixed(void);
+    Fixed(Fixed const & src);
+    Fixed(int const value);
+    Fixed(float const value);
+    ~Fixed(void);
+
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+    Fixed & operator=(Fixed const & src);
+
+    private:
+
+    int              _fixValue;
+    static int const _bits = 8;
+};
+
+#endif
