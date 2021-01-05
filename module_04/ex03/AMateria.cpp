21a0b830281df3e6cbf33721dc6b5cdf9ec21c06
macrespo42 <maxime.crespopro@gmail.com>
2021-01-05 17:18:53 +0100

    m04 ex03 AMateria class

---
+++ module_04/ex03/AMateria.cpp
@@ -0,0 +1,28 @@
+#include "AMateria.hpp"
+
+AMateria::AMateria(std::string const& type) : _xp(0), _type(type) {}
+
+AMateria::AMateria(AMateria const& src) {
+    *this = src;
+}
+
+AMateria::~AMateria(void) {}
+
+AMateria & AMateria::operator=(AMateria const& src) {
+    this->_xp = src._xp;
+    this->_type = src._type;
+    return *this;
+}
+
+std::string const& AMateria::geType(void) const {
+    return this->_type;
+}
+
+unsigned int AMateria::getXP(void) const {
+    return this->_xp;
+}
+
+void AMateria::use(ICharacter& target) {
+    std::string name("Random"); // TMP
+    std::cout << name << " use unknown materia -__-" << std::endl;
+}
---
+++ module_04/ex03/AMateria.hpp
@@ -0,0 +1,27 @@
+#ifndef AMATERIA
+#define AMATERIA
+
+#include <iostream>
+#include <string>
+
+class AMateria {
+
+    public:
+        AMateria(std::string const& type);
+        AMateria(AMateria const& src);
+        ~AMateria(void);
+        AMateria & operator=(AMateria const& src);
+        std::string const& geType(void) const;
+        unsigned int getXP(void) const;
+        virtual AMateria* clone(void) const = 0;
+        virtual void use(ICharacter& target);
+
+    protected:
+        unsigned int _xp;
+        std::string _type;
+
+    private:
+        AMateria(void);
+};
+
+#endif
