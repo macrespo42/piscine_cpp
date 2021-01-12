383c5c665dd7cb0fa19aad5eb7d75501595bcaf6
macrespo42 <maxime.crespopro@gmail.com>
2021-01-12 20:37:00 +0100

    m5 ex3 add tests to main

--- module_05/ex03/main.cpp
+++ module_05/ex03/main.cpp
@@ -3,22 +3,24 @@
 #include "ShrubberyCreationForm.hpp"
 #include "PresidentialPardonForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "Intern.hpp"
 
 int main(void) {
     Bureaucrat adam("Adam", 130);
     Bureaucrat boss("Boss", 1);
+    Intern someRandomIntern;
 
     std::cout << adam;
-    Form * houseTree = new ShrubberryCreationForm("House");
+    Form * houseTree = someRandomIntern.makeForm("Shruberry form", "House");
     houseTree->beSigned(adam);
     houseTree->execute(adam);
 
-    Form * cesar = new PresidentialPardonForm("Cesar");
+    Form * cesar = someRandomIntern.makeForm("Presidential pardon form" ,"Cesar");
 
     cesar->beSigned(boss);
     cesar->execute(boss);
 
-    Form * leo = new RobotomyRequestForm("Leo");
+    Form * leo = someRandomIntern.makeForm("Robotmy request form" ,"Leo");
 
     leo->beSigned(boss);
     leo->execute(boss);
