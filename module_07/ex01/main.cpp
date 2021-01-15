19c37a79254d050ff012b72ebfd661d9b992f3f5
macrespo42 <maxime.crespopro@gmail.com>
2021-01-15 16:37:41 +0100

    Improve main

--- module_07/ex01/main.cpp
+++ module_07/ex01/main.cpp
@@ -4,8 +4,10 @@ int main(void) {
 
     int arrayInt[3] = {4, 2, 3};
     float arrayFloat[3] = {4.2, 2.4, 3.3};
+    std::string array[3] = {"coucou", "cucu", "caca"};
 
     iter<int>(arrayInt, 3, print);
     iter<float>(arrayFloat, 3, print);
+    iter<std::string>(array, 3, print);
     return 0;
 }
