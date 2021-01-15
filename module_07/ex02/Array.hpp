b29c2e62657e409f26f54802c6aaa5af92415c62
macrespo42 <maxime.crespopro@gmail.com>
2021-01-15 16:57:45 +0100

    m7 ex2 review indent

--- module_07/ex02/Array.hpp
+++ module_07/ex02/Array.hpp
@@ -11,6 +11,7 @@ class Array {
         Array(void) : _size(0) {
             this->_array = new T[this->_size];
         }
+
         Array(unsigned int n) : _size(n) {
             T *tmp = new T();
             this->_array = new T[this->_size];
@@ -18,21 +19,26 @@ class Array {
                 this->_array[i] = *tmp;
             delete tmp;
         }
+
         Array(Array const& src) {
             *this = src;
         }
+
         ~Array(void) {
             delete [] this->_array;
         }
+
         Array<T> & operator=(Array const& src) {
             this->_array = src._array;
             this->_size = src._size;
             for (unsigned int i(0); i < this->_size; i++)
                 this->_array[i] = src._array[i];
         }
+
         unsigned int size(void) const {
             return this->_size;
         }
+
         T& operator[](int index) {
             if (index < 0 || index > this->_size)
                 throw std::out_of_range ("Index out of range"); 
