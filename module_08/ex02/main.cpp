520461f9c9f73f54f564855a66bf2ab912a79970
macrespo42 <maxime.crespopro@gmail.com>
2021-02-05 11:46:16 +0100

    m8 ex2 add tests to main

--- module_08/ex02/main.cpp
+++ module_08/ex02/main.cpp
@@ -5,14 +5,15 @@ int main()
     Mutantstack<int> mstack;
     mstack.push(5);
     mstack.push(17);
-    std::cout << "On top of the stack :" << mstack.top() << std::endl;
+    std::cout << "On top of the stack : " << mstack.top() << std::endl;
     mstack.pop();
-    std::cout << "Stack size : "<< mstack.size() << std::endl;
+    std::cout << "Stack size after pop : "<< mstack.size() << std::endl;
+    std::cout << "On top of the stack : "<< mstack.top() << std::endl;
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-    //[...]
     mstack.push(0);
+    std::cout << "Stack size after 4 push : " << mstack.size() << std::endl;
     Mutantstack<int>::iterator it = mstack.begin();
     Mutantstack<int>::iterator ite = mstack.end();
     ++it;
@@ -21,6 +22,14 @@ int main()
     {
         std::cout << "Stack value : " << *it << std::endl;
         ++it;
+    }
+    std::cout << "----------------------------------------------------------------" << std::endl;
+    Mutantstack<int>::reverse_iterator rit = mstack.rbegin();
+    Mutantstack<int>::reverse_iterator rite = mstack.rend();
+    while (rit != rite)
+    {
+        std::cout << "Stack reverse value : " << *rit << std::endl;
+        *rit++;
     }
     std::stack<int> s(mstack);
     return 0;
